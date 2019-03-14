e07c98b447d8017357aacf63a76a9f425b918c9e
Biplab <biplab@iotegral.in>
2019-03-14 19:56:18 +0530

    Update model

---
+++ Model/VehicleFeature.cs
@@ -0,0 +1,9 @@
+namespace vega.Model
+{
+    public class VehicleFeature
+    {
+        public int VehicleId { get; set; }
+        public int FeatureId { get; set; }
+        public Vehicle Vehicle { get; set; }
+    }
+}
