823a40b5ed42b7d744a63446d54a37f594430589
Amaresh <amaresh_adak@yahoo.com>
2019-03-15 09:50:31 +0530

    Add Vehicle

--- Model/VehicleFeature.cs
+++ Model/VehicleFeature.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Viga.Model;
+
 namespace vega.Model
 {
+    [Table("VehicleFeatures")]
     public class VehicleFeature
     {
         public int VehicleId { get; set; }
         public int FeatureId { get; set; }
         public Vehicle Vehicle { get; set; }
+        public Feature Feature { get; set; }
     }
 }
--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -1,13 +1,23 @@
 using Microsoft.EntityFrameworkCore;
+using vega.Model;
 using Viga.Model;
 
 namespace Viga.Persistence
 {
     public class VegaDbContext : DbContext
     {
-        public VegaDbContext(DbContextOptions<VegaDbContext> option):base(option)
-        { }
         public DbSet<Make> Makes { get; set; }
         public DbSet<Feature> Features { get; set; }
+        public VegaDbContext(DbContextOptions<VegaDbContext> option):base(option)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(
+                vf =>
+                new { vf.VehicleId, vf.FeatureId}
+            );
+        }
+        
     }
 }
